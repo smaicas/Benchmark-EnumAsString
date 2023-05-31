8ce288c0e5f53b9ae0515c3e09ed96b89fd040e6
Samuel Maicas Loma Osorio <SML@cyc.es>
2023-05-31 13:57:57 +0200

    .

--- Program.cs
+++ Program.cs
@@ -2,6 +2,20 @@
 using BenchmarkDotNet.Order;
 using BenchmarkDotNet.Running;
 
+CheckSameResult();
+
+void CheckSameResult()
+{
+    string val = MyEnum.EnumValue.ToString();
+    Console.WriteLine("ToString: " + val);
+    Console.WriteLine($"String interpolation: {MyEnum.EnumValue}");
+    Console.WriteLine($"Nameof: {nameof(MyEnum.EnumValue)}");
+    Console.WriteLine($"ToString: {Enum.GetName(MyEnum.EnumValue)}");
+}
+
+Console.WriteLine($"Press key to start");
+
+Console.ReadKey();
 BenchmarkDotNet.Reports.Summary summary = BenchmarkRunner.Run<EnumAsStringBenchmark>();
 
 Console.ForegroundColor = ConsoleColor.Cyan;
@@ -9,18 +23,18 @@ Console.WriteLine("Benchmark ended!. Press any key to close");
 Console.ResetColor();
 Console.ReadKey();
 
+enum MyEnum
+{
+    EnumValue
+}
+
 [RankColumn]
 [Orderer(SummaryOrderPolicy.FastestToSlowest)]
 [MemoryDiagnoser]
 public class EnumAsStringBenchmark
 {
     private const int Iterations = 10000;
-    private string Value { get; set; } = string.Empty;
-
-    private enum MyEnum
-    {
-        EnumValue
-    }
+    private string? Value { get; set; } = string.Empty;
 
     [Benchmark]
     public void UsingToString()
@@ -28,6 +42,16 @@ public class EnumAsStringBenchmark
         for (int i = 0; i < Iterations; i++)
         {
             string value = MyEnum.EnumValue.ToString();
+            Value = value;
+        }
+    }
+
+    [Benchmark]
+    public void UsingStringInterpolation()
+    {
+        for (int i = 0; i < Iterations; i++)
+        {
+            string value = $"{MyEnum.EnumValue}";
             Value = value;
         }
     }
@@ -41,6 +65,17 @@ public class EnumAsStringBenchmark
             Value = value;
         }
     }
+
+    [Benchmark]
+    public void UsingEnumGetName()
+    {
+        for (int i = 0; i < Iterations; i++)
+        {
+            string? value = Enum.GetName(MyEnum.EnumValue);
+            Value = value;
+        }
+    }
+
     [Benchmark]
     public void UsingToStringDiffers()
     {
@@ -52,11 +87,31 @@ public class EnumAsStringBenchmark
     }
 
     [Benchmark]
+    public void UsingStringInterpolationDiffers()
+    {
+        for (int i = 0; i < Iterations; i++)
+        {
+            string value = $"{MyEnum.EnumValue}";
+            Value = value;
+        }
+    }
+
+    [Benchmark]
     public void UsingNameOfDiffers()
     {
         for (int i = 0; i < Iterations; i++)
         {
             string value = nameof(MyEnum.EnumValue);
+            Value = value;
+        }
+    }
+
+    [Benchmark]
+    public void UsingEnumGetNameDiffers()
+    {
+        for (int i = 0; i < Iterations; i++)
+        {
+            string? value = Enum.GetName(MyEnum.EnumValue);
             Value = value;
         }
     }
